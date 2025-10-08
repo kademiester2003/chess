2e904c96b0e95882e9da71d4efc149abd5101639
Kaden Kartchner <kad3n@byu.edu>
2025-10-08 15:09:33 -0600

    Passed registerSuccess test

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
@@ -10,10 +14,18 @@ public class Server {
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
         server.delete("db", ctx -> ctx.result( "{}"));
-        server.post("user", ctx -> ctx.result("{\"username\":\"joe\", \"authToken\":\"xyz\"}"));
+        server.post("user", this::register);
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        req.put("authToken", "cow");
+        var res = serializer.toJson(req);
+        ctx.result(res);
     }
 
     public int run(int desiredPort) {
