d8cc05a2bb629dca0527d96b0e43565a33d771e1
Kaden Kartchner <kad3n@byu.edu>
2025-10-30 00:16:57 -0600

    Loosely began to implement User data logic in MySQLDataAccess.java

--- server/src/main/java/dataaccess/MySQLDataAccess.java
+++ server/src/main/java/dataaccess/MySQLDataAccess.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import com.google.gson.Gson;
+import model.User;
 
 import java.sql.SQLException;
 
@@ -47,6 +48,55 @@ public class MySQLDataAccess implements DataAccess {
             }
         } catch (SQLException ex) {
             throw new DataAccessException("failed to create tables", ex);
+        }
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection();
+             var statement = conn.createStatement()) {
+            statement.executeUpdate("DELETE FROM Auths");
+            statement.executeUpdate("DELETE FROM Games");
+            statement.executeUpdate("DELETE FROM Users");
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to clear database", ex);
+        }
+    }
+
+    @Override
+    public void createUser(User user) throws DataAccessException {
+        if (user == null) {
+            throw new DataAccessException("null user");
+        }
+        final String sql = "INSERT INTO Users (username, password, email) VALUES (?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, user.username());
+            ps.setString(2, user.password());
+            ps.setString(3, user.email());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            if (ex.getSQLState() != null && (ex.getSQLState().startsWith("23") || ex.getErrorCode() == 1062)) {
+                throw new DataAccessException("user exists", ex);
+            }
+            throw new DataAccessException("failed to create user", ex);
+        }
+    }
+
+    @Override
+    public User getUser(String username) throws DataAccessException {
+        final String sql = "SELECT username, password, email FROM users WHERE username = ?";
+        try (var conn = DatabaseManager.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            ps.setString(1, username);
+            try (var result = ps.executeQuery()) {
+                if (!result.next()) {
+                    return null;
+                }
+                return new User(result.getString("username"), result.getString("password"), result.getString("email"));
+            } catch (SQLException ex) {
+                throw new DataAccessException("failed to get user", ex);
+            }
         }
     }
 
