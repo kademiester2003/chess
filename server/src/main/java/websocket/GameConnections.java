22d071ca433f4ef0eba793ec2889e59ac62e0daa
Kaden Kartchner <kad3n@byu.edu>
2025-12-01 22:49:02 -0700

    Error message implemented. GameWebSocketEndpoint and GameConnections started to implemented

--- server/src/main/java/websocket/GameConnections.java
+++ server/src/main/java/websocket/GameConnections.java
@@ -1,4 +1,24 @@
 package websocket;
 
+import com.google.gson.Gson;
+import jakarta.websocket.Session;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class GameConnections {
+    private static final Gson gson = new Gson();
+    private final int gameID;
+    private final ConcurrentHashMap<Session, String> sessions = new ConcurrentHashMap<>();
+
+    public GameConnections(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public void addSession(Session session, String username) {
+        sessions.put(session, username);
+    }
+
+    public void removeSession(Session session, String username) {
+        sessions.remove(session, username);
+    }
 }
--- server/src/main/java/websocket/GameWebSocketEndpoint.java
+++ server/src/main/java/websocket/GameWebSocketEndpoint.java
@@ -1,4 +1,104 @@
 package websocket;
 
+import com.google.gson.*;
+import com.google.gson.GsonBuilder;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import dataaccess.MySQLDataAccess;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+import jakarta.websocket.*;
+import model.Auth;
+import model.Game;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+
 public class GameWebSocketEndpoint {
+    private static final Gson gson = new GsonBuilder().create();
+    private static final ConcurrentHashMap<Integer, GameConnections> games = new ConcurrentHashMap<>();
+    private static final DataAccess dao;
+    static {
+        DataAccess tmp;
+        try {
+            tmp = new MySQLDataAccess();
+        } catch (DataAccessException ex) {
+            tmp = new MemoryDataAccess();
+        }
+        dao = tmp;
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {}
+
+    @OnMessage
+    public void onMessage(Session session, String msg) {
+        JsonObject root;
+        try {
+            root = JsonParser.parseString(msg).getAsJsonObject();
+        } catch (JsonParseException | IllegalStateException ex) {
+            sendError(session, "error: invalid JSON");
+            return;
+        }
+
+        JsonElement ctEl = root.get("commandType");
+        if (ctEl == null) {
+            sendError(session, "error: missing commandType");
+            return;
+        }
+
+        String ctStr =  ctEl.getAsString();
+        switch (ctStr) {}
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        games.values().forEach(gc -> gc.removeSession(session));
+    }
+
+    @OnError
+    public void onError(Session session, Throwable thr) {}
+
+    private void handleConnect(Session session, UserGameCommand cmd) {
+        if (cmd == null) {
+            sendError(session, "error: invalid command");
+            return;
+        }
+        Integer gameID = cmd.getGameID();
+        String token = cmd.getAuthToken();
+
+        try {
+            Auth auth = dao.getAuth(token);
+            if (auth == null) {
+                sendError(session, "error: invalid auth token");
+                return;
+            }
+
+            Game game = dao.getGame(gameID);
+            if (game == null) {
+                sendError(session, "error: game not found");
+                return;
+            }
+
+            GameConnections gc = games.computeIfAbsent(gameID, id -> new GameConnections(gameID));
+        } catch (DataAccessException ex) {
+            sendError(session, "error: server data error");
+        }
+    }
+
+    private void sendError(Session session, String msg) {
+        try {
+            ErrorMessage em = new ErrorMessage(msg);
+            sendJson(session, em);
+        } catch (Exception ex) {}
+    }
+
+    private void sendJson(Session session, ErrorMessage msg) {
+        try {
+            String json = gson.toJson(msg);
+            session.getBasicRemote().sendText(json);
+        } catch (IOException ex) {}
+    }
 }
