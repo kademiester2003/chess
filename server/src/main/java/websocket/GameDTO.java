9877e8bbe238d4335dfe920823fce2913879f595
Kaden Kartchner <kad3n@byu.edu>
2025-12-01 23:28:27 -0700

    implemented GameDTO

--- server/src/main/java/websocket/GameDTO.java
+++ server/src/main/java/websocket/GameDTO.java
@@ -1,8 +1,24 @@
 package websocket;
 
+import chess.ChessGame;
 import model.Game;
 
 public class GameDTO {
-    public static Object fromModel(Game game) {
+    public int gameID;
+    public String whiteUsername;
+    public String blackUsername;
+    public String gameName;
+    public ChessGame game;
+    public String status;
+
+    public static GameDTO fromModel(Game model) {
+        GameDTO dto = new GameDTO();
+        dto.gameID = model.gameID();
+        dto.whiteUsername = model.whiteUsername();
+        dto.blackUsername = model.blackUsername();
+        dto.gameName = model.gameName();
+        dto.game = model.game();
+        dto.status = "ONGOING";
+        return dto;
     }
 }
